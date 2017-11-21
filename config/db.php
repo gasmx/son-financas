21f30bc30dce95d03962774565781d0fd687d789
Gabriel Angelus <gangeluscf@gmail.com>
2017-11-20 22:11:19 -0200

    Primeiro commit

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'dev' => [
+        'driver' => 'mysql',
+        'host' => 'localhost',
+        'database' => 'son_financas',
+        'username' => 'root',
+        'password' => '123456789',
+        'charset' => 'utf8',
+        'collation' => 'utf8_unicode_ci'
+    ]
+];
